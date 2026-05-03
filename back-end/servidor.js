5da1c64c5d0a5fcefd4927ab0399a45c190acced
Silvano Constantino <silvanoconstantinopro@gmail.com>
2026-05-03 02:22:12 +0100

    yh

--- back-end/servidor.js
+++ back-end/servidor.js
@@ -9,13 +9,7 @@ import { configurarWebSocket } from "./websocket.js";
 // ✅ IMPORTAÇÃO E INICIALIZAÇÃO ÚNICA PARA PRISMA v7.x
 import { PrismaClient } from "./generated/prisma/index.js";
 // ✅ Forma correta para a versão v7.8.0
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
+const prisma = new PrismaClient();
 
 // Importando as rotas
 import { routerUsuarios } from "./rotas/rotasUsuario.js";
